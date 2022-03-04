fee2f6e5c578561e1156bb68418249ff757feb58
Luca Illica Magrini <86322780+LUCAIM08@users.noreply.github.com>
2022-03-04 19:31:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ const client = new Discord.Client({
     intents: 32767
 })
 
-const disbut = require("discord-buttons")
-disbut(client);
-
-const { MessageButton, MessageActionRow } = require("discord-buttons")
-const { MessageMenuOption, MessageMenu } = require("discord-buttons")
-
 client.once("ready", () => {
     console.log("Ready")
 })
@@ -20,28 +14,17 @@ client.on("messageCreate", message => {
             if(!task) {
                 message.reply("Usage `t!addtask [Task Description]`!")
             }else {
-                var button1 = new MessageButton()
-                    .setLabel("Done")
-                    .setStyle("green")
-                    .setID("done")
-                var button2 = new MessageButton()
-                    .setLabel("Ciao")
-                    .setStyle("red")
-                    .setID("reject")
-
-                var row = new MessageActionRow()
-                    .addComponent(button1)
-                    .addComponent(button2)
-                
                 const embed = new Discord.MessageEmbed()
                 .setColor("DARK_GREEN")
                 .setTitle("New Task!")
                 .setDescription(task)
-                .addField("Complete", "Incoming", true)
-                .addField("Reject", "Incoming", false)
-                .setFooter(`Given by ${message.author.username}#${message.author.discriminator}`)
+                .addField("\✅", "Incoming", true)
+                .addField("\❌", "Incoming", false)
 
-                message.channel.send({embed: [embed]})
+                message.channel.send({embeds: [embed]}).then(msg => {
+                    msg.react("✅")
+                    msg.react("❌")
+                })
             }
     }
 })
